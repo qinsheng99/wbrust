70fe0ce585ad5994d437cdd83c25867348398b2a
zjm <1076115376@qq.com>
2023-04-20 14:16:05 +0800

    wb post

--- src/controller/greet.rs
+++ src/controller/greet.rs
@@ -1,19 +1,37 @@
 use actix_web::{web, HttpResponse, Resource, Responder};
+use http::StatusCode;
 use serde::ser::SerializeStruct;
 use serde::{Deserialize, Serialize, Serializer};
 use serde_json::Result as SerdeResult;
 
 #[allow(dead_code)]
-async fn greet(name: web::Path<String>) -> impl Responder {
+async fn get_greet(name: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(name.into_inner())
 }
 
-pub fn score() -> Resource {
-    web::resource("/hello/{name}").route(web::get().to(greet))
+#[allow(dead_code)]
+async fn post_greet(name: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(mime::APPLICATION_JSON)
+        .json(Greet::new_success(name.into_inner().as_str()))
+}
+
+#[allow(dead_code)]
+pub fn score() -> Vec<Resource> {
+    let mut r: Vec<Resource> = vec![];
+
+    r.push(
+        web::resource("/hello/{name}")
+            .route(web::get().to(get_greet))
+            .route(web::post().to(post_greet)),
+    );
+
+    r
 }
 
 #[derive(Deserialize)]
 struct Greet {
+    pub code: u16,
     pub data: String,
 }
 
@@ -22,6 +40,23 @@ impl Greet {
     fn default() -> Self {
         Greet {
             data: String::new(),
+            code: StatusCode::default().as_u16(),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn new_success(data: &str) -> Self {
+        Greet {
+            data: data.to_string(),
+            code: StatusCode::OK.as_u16(),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn new(data: &str, code: u16) -> Self {
+        Greet {
+            data: data.to_string(),
+            code,
         }
     }
 
@@ -42,6 +77,7 @@ impl Serialize for Greet {
         T: Serializer,
     {
         let mut s = serializer.serialize_struct("Greet", 1)?;
+        s.serialize_field("code", &self.code)?;
         s.serialize_field("data", &self.data)?;
         s.end()
     }
